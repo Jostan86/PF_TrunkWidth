00b24a5055d3ab7683abdabf5b2ea5f6ae70178f
jostan86 <jostanbrown86@gmail.com>
2023-10-06 11:21:04 -0700

    commit small changes

--- scripts/env_vars.py
+++ scripts/env_vars.py
@@ -10,3 +10,5 @@ os.environ['MAP_DATA_PATH'] = (pkg_dir + 'data/tree_list_mod4.json')
 # os.environ['MODEL_WEIGHT_PATH'] = "/home/jostan/OneDrive/Docs/Grad_school/Research/yolo_model/best_x_500_v7.pt"
 os.environ['MODEL_WEIGHT_PATH'] = (pkg_dir + "/data/best_s_500_v7.pt")
 os.environ['MODEL_STARTUP_IMAGE_PATH'] = (pkg_dir + "/data/live_startup_image.png")
+
+# os.environ['LD_PRELOAD'] = '/usr/lib/x86_64-linux-gnu/libstdc++.so.6'
